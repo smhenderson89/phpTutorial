583985f069a394473430ee0539cc65d0428e5cab
Scott Henderson <scott.mc.henderson@gmail.com>
2022-05-10 16:07:23 -0700

    function require/include

--- 10_include_require/01_website_skeleton/index.php
+++ 10_include_require/01_website_skeleton/index.php
@@ -1,8 +1,5 @@
 <?php include_once "partials/header.php"; ?>
 <h3>Georgia, Tbilisi 5&#8451;</h3>
 <h1>Welcome to my cool website</h1>
-<footer>
-    Copyright &copy; 2020 TheCodeholic
-</footer>
 </body>
 </html>
--- 10_include_require/02_functions/index.php
+++ 10_include_require/02_functions/index.php
@@ -1,1 +1,6 @@
 <?php
+
+require_once "math.php";
+
+echo add(4, 5).'<br>';
+echo subtract(5,4).'<br>';
