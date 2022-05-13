a85c2f95dc6124dfdd5d6bf064770b6bbfee52c2
Scott Henderson <scott.mc.henderson@gmail.com>
2022-05-13 16:41:09 -0700

    oop_complete

--- 12_oop/Student.php
+++ 12_oop/Student.php
@@ -1,8 +1,16 @@
 <?php 
 
 require_once "Person.php";
-class Student extends Person{
+class Student extends Person
+{
+    public string $studentId;
 
+    public function __construct($name, $surname, $studentId) 
+    {
+        parent::__construct($name, $surname);
+        $this ->age = 18;
+        $this->studentId = $studentId;
+    }
 }
 
 ?>
--- 12_oop/index.php
+++ 12_oop/index.php
@@ -2,19 +2,24 @@
 
 // What is class and instance?
 // require_once "Person.php";
+require_once "Person.php";
 require_once "Student.php";
 
-echo 'new file locations hooray it works!'.'<br>';
+$student = new Student("Brad", "Traversy", '1234');
+echo '<prev>';
+var_dump($student);
+echo '</prev>';
+// echo 'new file locations hooray it works!'.'<br>';
 
-$p = new Person("Brad", "Traversy");
-$p->setAge(30);
-echo $p->getAge().'<br>';
-echo json_encode($p).'<br>'; 
+// $p = new Person("Brad", "Traversy");
+// $p->setAge(30);
+// echo $p->getAge().'<br>';
+// echo json_encode($p).'<br>'; 
 
-$p2 = new Person('John', 'Smith');
-echo json_encode($p2).'<br>';
+// $p2 = new Person('John', 'Smith');
+// echo json_encode($p2).'<br>';
 
-echo Person::$counter; // "::" access static, constant or overriden properties or methods of a class
+// echo Person::$counter; // "::" access static, constant or overriden properties or methods of a class
 
 // Create Person class in Person.php
 
