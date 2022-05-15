a8b5484f65fc9f310d6443d3f486af6b1a93e1c2
Scott Henderson <scott.mc.henderson@gmail.com>
2022-05-15 12:06:21 -0700

    CRUD beginner work in progress

--- 14_product_crud/create.php
+++ 14_product_crud/create.php
@@ -6,6 +6,42 @@ $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud','root','');
 // Check if connection was successful
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+// $pdo->exec("INSERT INTO products (title, image, description, price, create_date)
+//                 VALUE('$title','','$description',$price,'$date')
+// ") 
+// Beginner way of putting a post into the database
+
+$errors = []; // Add to errors for troubleshooting
+
+// echo $_SERVER['REQUEST_METHOD'].'<br>';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $date = date('Y-m-d H:i:s');
+
+
+    if(!$title) {
+        $errors[] = 'Product title is required';
+    }
+
+    if (!$price) {
+        $errors[] = 'Product price is required';
+    }
+
+    // Better way - Check if request method is get or POST?
+    $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
+                    VALUE(:title, :image, :description, :price, :date)"); 
+
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':image', '');
+    $statement->bindValue(':description', $description);
+    $statement->bindValue(':price', $price);
+    $statement->bindValue(':date', $date);
+    $statement->execute();
+}
+
 ?>
 
 <!doctype html>
@@ -19,22 +55,34 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   </head>
   <body>
     <h1>Create New Product</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-        <form>
+        <?php if (!empty($errors)): ?>
+            <div class="alert alert-danger" role="alert">
+
+                <?php foreach ($errors as $error): ?>
+                    <div><?php echo $error ?></div>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+        <form action="" method = "post"> <!-- Where form will be submitted and which method -->
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <label>Product Image</label>
+                <br>
+                <input type = "file" name = "image"> 
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label>Product Title</label>
+                <input type = "text" name = "title" class="form-control"> 
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <div class="mb-3">  
+                <label>Product Description</label>
+                <textarea name = "description" class="form-control"></textarea>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="mb-3">
+                <label>Product Price</label>
+                <input type = "number" name = "price" step="0.01" class="form-control"> 
+            </div>
+            <button type="submit" id = "submit" class="btn btn-primary">Submit</button>
         </form>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     </body>
 </html>
